9d46742f4a25cd31bfd0d7e10ff7ed5ac10d9a42
Matteo Conti <matteo.conti91@gmail.com>
2024-02-03 12:32:41 +0100

    add first set of test and fix Aqua errors

--- src/GeoGrids.jl
+++ src/GeoGrids.jl
@@ -10,4 +10,6 @@ include("mesh_func.jl")
 
 export fibonaccigrid, meshgrid
 
+# export plot_unitarysphere, plot_geo
+
 end # module GeoGrids
--- test/general.jl
+++ test/general.jl
@@ -6,59 +6,19 @@ using GeoGrids
 
 using PlotlyBase
 
-initEpoch = now()
-initEpoch_julian = initEpoch |> datetime2julian
-orb_file_contents = strip("""
-|   ConstID   |   Epoch   |   a   |   e   |   i   |   RAAN   |   per   |   M   |
-6	$initEpoch_julian	7.378137e6	0.0	90.0	0.0	0.0	0.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	0.0	0.0	72.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	0.0	0.0	144.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	0.0	0.0	216.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	0.0	0.0	288.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	90.0	0.0	36.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	90.0	0.0	108.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	90.0	0.0	180.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	90.0	0.0	252.0
-6	$initEpoch_julian	7.378137e6	0.0	90.0	90.0	0.0	324.0
-2	$initEpoch_julian	6.878137e6	0.0	70.0	0.0	0.0	0.0
-2	$initEpoch_julian	6.878137e6	0.0	70.0	0.0	0.0	90.0
-2	$initEpoch_julian	6.878137e6	0.0	70.0	0.0	0.0	180.0
-2	$initEpoch_julian	6.878137e6	0.0	70.0	0.0	0.0	270.0
-""")
-
-satInits_constellation = mktempdir() do dir # We generate the vector of subconstellations data
-    cd(dir) do
-        open("orbfile.orb", "w") do io
-            write(io, orb_file_contents)
-        end
-        read_kepleriansFile("orbfile.orb")
-    end
+@testset "Plots Unitary Sphere" begin
+    points1 = GeoGrids.fibonaccisphere_classic(100; coord=:cart)
+    GeoGrids.plot_unitarysphere(points1)
 end
 
-em = EllipsoidModel(WGS84_ELLIPSOID)
-sats = sat_from_init(satInits_constellation; em)
+# @testset "Plots Plotly Base" begin
+#     @test GeoGrids.plot_geo(map(x -> rad2deg.(x), fibonaccigrid(sepAng=deg2rad(4)))) isa Plot
+#     @test GeoGrids.plot_geo(map(x -> rad2deg.(x), fibonaccigrid(sepAng=deg2rad(4)));camera=:threedim) isa Plot
+# end
 
-@testset "Main Function" begin
-    @test plotSat_scattergeo(sats) isa Plot
-    @test plotSat_scattergeo(sats; eta_fov=deg2rad(30), res_fov=0.2, title="Test", camera=:threedim, sat_marker_size=4) isa Plot
-    @test plotSat_scattergeo(sats; res_fov=0.2, sat_marker_size=4) isa Plot
-    @test plotSat_scattergeo(sats; eta_fov=deg2rad(30), res_fov=0.2) isa Plot
-end
+# using PlutoPlotly
 
-@testset "Function t_instant" begin
-    @test plotSat_scattergeo(sats, 60) isa Plot
-    @test plotSat_scattergeo(sats, 60;eta_fov=deg2rad(30), res_fov=0.2, title="Test", camera=:threedim, sat_marker_size=4) isa Plot
-    @test plotSat_scattergeo(sats, 60;res_fov=0.2, sat_marker_size=4) isa Plot
-    @test plotSat_scattergeo(sats, 60;eta_fov=deg2rad(30), res_fov=0.2) isa Plot
-end
-
-@testset "Function t interval" begin
-    @test plotSat_scattergeo(sats, (0,60,1200)) isa Plot
-    @test plotSat_scattergeo(sats, (0,60,1200); eta_fov=deg2rad(30), res_fov=0.2, title="Test", camera=:threedim, sat_marker_size=4) isa Plot
-    @test plotSat_scattergeo(sats, (0,60,1200); res_fov=0.2, sat_marker_size=4) isa Plot
-    @test plotSat_scattergeo(sats, (0,60,1200); eta_fov=deg2rad(30), res_fov=0.2) isa Plot
-end
-
-using PlutoPlotly
-
-@test plotSat_scattergeo(sats, (0,60,1200)) isa PlutoPlot
+# @testset "Plots Plotly Base" begin
+#     @test GeoGrids.plot_geo(map(x -> rad2deg.(x), fibonaccigrid(sepAng=deg2rad(4)))) isa PlutoPlot
+#     @test GeoGrids.plot_geo(map(x -> rad2deg.(x), fibonaccigrid(sepAng=deg2rad(4)));camera=:threedim) isa PlutoPlot
+# end
