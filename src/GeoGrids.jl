927fee21ba144e669b27d5f3fe8bda2fbe0cd9f5
Matteo Conti <matteo.conti@esa.int>
2024-06-01 11:19:39 +0200

    add Region

--- src/GeoGrids.jl
+++ src/GeoGrids.jl
@@ -5,6 +5,7 @@ using StaticArrays
 using AngleBetweenVectors
 using PlotlyExtensionsHelper
 
+include("filtering.jl")
 include("plot_func.jl")
 include("fibonacci_func.jl")
 include("mesh_func.jl")
---
+++ src/filtering.jl
@@ -0,0 +1,6 @@
+@kwdef mutable struct Region
+    regionName::String = "region_name"
+    continent::String = ""
+    subregion::String = ""
+    admin::String = ""
+end
