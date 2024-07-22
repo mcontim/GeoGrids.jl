5550b0ff86075f96f47667953795bcfcd57f55ab
Matteo Conti <matteo.conti91@gmail.com>
2024-07-22 15:14:00 +0200

    update vecgrid

--- src/mesh_func.jl
+++ src/mesh_func.jl
@@ -14,7 +14,7 @@ The meshgrid cover all the globe with LAT=-90:90 and LON=-180:180
 ## Output
 - `out`: Matrix{Union{LLA,Point2}}, each element of the matrix is either a `LLA` or `Point2`. The order of the elements is LAT, LON.
 """
-function rectgrid(xRes::ValidAngle; yRes::ValidAngle=xRes, height=nothing, unit=:rad, type=:lla)
+function rectgrid(xRes::ValidAngle; yRes::ValidAngle=xRes)
 	# Input Validation   
 	_xRes = let
 		x = xRes isa Real ? xRes * ° : l # Convert to Uniful
@@ -39,9 +39,9 @@ function rectgrid(xRes::ValidAngle; yRes::ValidAngle=xRes, height=nothing, unit=
 	end
 
 	# Create the rectangular grid of elements SimpleLatLon
-	out = [SimpleLatLon(x,y) for x in -90°:_xRes:90°, y in -180°:_yRes:(180°-_yRes+1e-10*°)]
+	mat = [SimpleLatLon(x,y) for x in -90°:_xRes:90°, y in -180°:_yRes:(180°-_yRes+1e-10*°)]
 
-	return out
+	return mat
 end
 
 """
@@ -58,24 +58,22 @@ Create a vector grid with a given resolution.
 ## Returns
 - `out`: The output vector grid. If `type` is `:lla`, each element of the grid is an `LLA` object with latitude, longitude, and altitude. If `type` is `:point`, each element of the grid is a `Point2` object with latitude and longitude.
 """
-function vecgrid(gridRes::ValidAngle; height=nothing, unit=:rad, type=:lla)
+function vecgrid(gridRes::ValidAngle)
 	# Input Validation
 	_gridRes = let
-		_check_angle(gridRes; limit=π/2, msg = "Resolution of grid is too large, it must be smaller than π/2...")
-		if gridRes < 0
+		x = gridRes isa Real ? gridRes * ° : l # Convert to Uniful
+		abs(x) ≤ 90° || error("Resolution of grid is too large, it must be smaller than π...")
+		if x < 0
 			@warn "Input gridRes is negative, it will be converted to positive..."
-			to_radians(abs(gridRes))
+			abs(x)
 		else
-			to_radians(gridRes)
-		end	
+			x
+		end
 	end
 
 	# Create LAT vector
-	temp = collect(0:_gridRes:π/2) # LAT vector from 0 to π/2
-	vec = map(x -> SVector(x, 0.0), temp) # LAT vector from 0 to π/2
+	temp = collect(0:_gridRes:90°) # LAT vector from 0 to π/2
+	vec = map(x -> SimpleLatLon(x, 0.0), temp) # LAT vector from 0 to π/2
 	
-	# Unit Conversion
-	out = _grid_points_conversion(vec; height, type, unit)
-
-	return out
+	return vec
 end
