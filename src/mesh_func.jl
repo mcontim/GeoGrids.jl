a898f79b009689383a4feffe95299ae10654ff0c
Matteo Conti <matteo.conti91@gmail.com>
2024-07-22 15:19:12 +0200

    documentation update

--- src/mesh_func.jl
+++ src/mesh_func.jl
@@ -1,79 +1,89 @@
 """
-	rectgrid(xRes::ValidAngle; yRes::ValidAngle=xRes, height=nothing, unit=:rad, type=:lla)
+    rectgrid(xRes::ValidAngle; yRes::ValidAngle=xRes) -> Array{SimpleLatLon, 2}
 
-This function call meshgrid with the specified resolution given as input and return the LAT, LON meshgrid (LAT=y, LON=x).
-The meshgrid cover all the globe with LAT=-90:90 and LON=-180:180
+Create a rectangular grid of latitude and longitude points with a specified grid
+resolutions. The function validates the input resolutions to ensure they are
+within the range of `-180°` to `180°`. If a negative resolution is provided, it
+is converted to a positive value with a warning. The resolution values are used
+to create a rectangular grid covering the entire range of latitudes `[-90°,
+90°]` and longitudes `[-180°, 180°)`. 
 
 ## Arguments
-- `xRes`: resolution of the x axis in meshgrid in `ValidAngle`. 
-- `yRes`: resolution of the y axis in meshgrid in `ValidAngle`. 
-- `unit`: `:rad` or `:deg`
-- `type`: `:lla` or `:point`. Output type either `LLA` or `Point2`
-- `height`: the point altitude in case of LLA type
+- `xRes::ValidAngle`: The resolution for the latitude grid spacing. This can be \
+a real number (interpreted as degrees) or a `ValidAngle`.
+- `yRes::ValidAngle`: The resolution for the longitude grid spacing. This is \
+optional and defaults to `xRes` if not provided. This can be a real number \
+(interpreted as degrees) or a `ValidAngle`.
 
-## Output
-- `out`: Matrix{Union{LLA,Point2}}, each element of the matrix is either a `LLA` or `Point2`. The order of the elements is LAT, LON.
+## Returns
+- A 2D array of `SimpleLatLon` objects representing the grid of latitude and \
+longitude points.
 """
 function rectgrid(xRes::ValidAngle; yRes::ValidAngle=xRes)
-	# Input Validation   
-	_xRes = let
-		x = xRes isa Real ? xRes * ° : l # Convert to Uniful
-		abs(x) ≤ 180° || error("Resolution of x is too large, it must be smaller than π...")
-		if x < 0
-			@warn "Input xRes is negative, it will be converted to positive..."
-			abs(x)
-		else
-			x
-		end
-	end
+    # Input Validation   
+    _xRes = let
+        x = xRes isa Real ? xRes * ° : l # Convert to Uniful
+        abs(x) ≤ 180° || error("Resolution of x is too large, it must be smaller than π...")
+        if x < 0
+            @warn "Input xRes is negative, it will be converted to positive..."
+            abs(x)
+        else
+            x
+        end
+    end
 
-	_yRes = let
-		x = yRes isa Real ? yRes * ° : l # Convert to Uniful
-		abs(x) ≤ 180° || error("Resolution of x is too large, it must be smaller than π...")
-		if x < 0
-			@warn "Input yRes is negative, it will be converted to positive..."
-			abs(x)
-		else
-			x
-		end
-	end
+    _yRes = let
+        x = yRes isa Real ? yRes * ° : l # Convert to Uniful
+        abs(x) ≤ 180° || error("Resolution of x is too large, it must be smaller than π...")
+        if x < 0
+            @warn "Input yRes is negative, it will be converted to positive..."
+            abs(x)
+        else
+            x
+        end
+    end
 
-	# Create the rectangular grid of elements SimpleLatLon
-	mat = [SimpleLatLon(x,y) for x in -90°:_xRes:90°, y in -180°:_yRes:(180°-_yRes+1e-10*°)]
+    # Create the rectangular grid of elements SimpleLatLon
+    mat = [SimpleLatLon(x, y) for x in -90°:_xRes:90°, y in -180°:_yRes:(180°-_yRes+1e-10*°)]
 
-	return mat
+    return mat
 end
 
 """
-    vecgrid(gridRes::ValidAngle; height=nothing, unit=:rad, type=:lla)
+    vecgrid(gridRes::ValidAngle) -> Vector{SimpleLatLon}
 
-Create a vector grid with a given resolution.
+Generate a vector of latitude points from the equator to the North Pole with a
+specified resolution. The function validates the input resolution to ensure it
+is within the range of `-90°` to `90°`. If a negative resolution is provided, it
+is converted to a positive value with a warning. The resolution value is then
+used to create a vector of latitude points ranging from `0°` to `90°` (the North
+Pole). Each latitude point is represented as a `SimpleLatLon` object with a
+fixed longitude of `0°`.
 
 ## Arguments
-- `gridRes::ValidAngle`: The resolution of the grid. If the input is negative, it will be converted to positive. The resolution must be smaller than π/2.
-- `height`: The height value. It is optional. If not provided, it will be set to 0 by default. This argument is ignored when `type` is set to `:point`.
-- `unit::Symbol`: The unit of the grid. It can be `:rad` for radians (default) or `:deg` for degrees. This argument is used only when `type` is set to `:point`.
-- `type::Symbol`: The type of the output. It can be `:lla` (default) for latitude-longitude-altitude or `:point` for 2D point.
+- `gridRes::ValidAngle`: The resolution for the latitude grid spacing. This can \
+be a real number (interpreted as degrees) or a `ValidAngle`.
 
 ## Returns
-- `out`: The output vector grid. If `type` is `:lla`, each element of the grid is an `LLA` object with latitude, longitude, and altitude. If `type` is `:point`, each element of the grid is a `Point2` object with latitude and longitude.
+- A vector of `SimpleLatLon` objects representing latitude points from the \
+equator (0°) to the North Pole (90°) with the specified resolution.
 """
 function vecgrid(gridRes::ValidAngle)
-	# Input Validation
-	_gridRes = let
-		x = gridRes isa Real ? gridRes * ° : l # Convert to Uniful
-		abs(x) ≤ 90° || error("Resolution of grid is too large, it must be smaller than π...")
-		if x < 0
-			@warn "Input gridRes is negative, it will be converted to positive..."
-			abs(x)
-		else
-			x
-		end
-	end
+    # Input Validation
+    _gridRes = let
+        x = gridRes isa Real ? gridRes * ° : l # Convert to Uniful
+        abs(x) ≤ 90° || error("Resolution of grid is too large, it must be smaller than π...")
+        if x < 0
+            @warn "Input gridRes is negative, it will be converted to positive..."
+            abs(x)
+        else
+            x
+        end
+    end
 
-	# Create LAT vector
-	temp = collect(0:_gridRes:90°) # LAT vector from 0 to π/2
-	vec = map(x -> SimpleLatLon(x, 0.0), temp) # LAT vector from 0 to π/2
-	
-	return vec
+    # Create LAT vector
+    temp = collect(0°:_gridRes:90°)
+    vec = map(x -> SimpleLatLon(x, 0°), temp) # LAT vector from 0° to 90°
+
+    return vec
 end
