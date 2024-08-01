a7649c5b17421ef2f5bffd764d8c538aa0db5b48
Matteo Conti <matteo.conti91@gmail.com>
2024-08-01 10:27:59 +0200

    add documentation tassellation functions

--- src/tessellation_func.jl
+++ src/tessellation_func.jl
@@ -1,4 +1,28 @@
-# Basic generator for regular lattice
+"""
+    _gen_regular_lattice(dx::T, dy, ds; x0=zero(T), y0=zero(T), M::Int=100, N::Int=M) where {T}
+
+The `_gen_regular_lattice` function generates a regular lattice of points in a
+two-dimensional space. The lattice is defined by its spacing in the x and y
+directions (`dx` and `dy`), an additional offset (`ds`), and optional starting
+positions (`x0` and `y0`). The lattice spans `2M + 1` rows and `2N + 1` columns
+centered around the origin.
+
+## Arguments
+- `dx::T`: The spacing between points in the x direction.
+- `dy::T`: The spacing between points in the y direction.
+- `ds::T`: The additional offset in the x direction per row.
+- `x0::T`: The x-coordinate of the starting position. Default is `zero(T)`.
+- `y0::T`: The y-coordinate of the starting position. Default is `zero(T)`.
+- `M::Int`: The number of points in the x direction from the center. Default is \
+100.
+- `N::Int`: The number of points in the y direction from the center. Default is \
+equal to `M`.
+
+## Returns
+- `Array{SVector{2,T},2}`: A 2D array of points represented as static vectors \
+(`SVector{2,T}`) from the `StaticArrays` package. Each point is in the form \
+`(x, y)`.
+"""    
 function _gen_regular_lattice(dx::T, dy, ds; x0=zero(T), y0=zero(T), M::Int=100, N::Int=M) where {T}
     # Function to generate x position as function of row,column number m,n
     x(m, n) = m * dx + n * ds + x0
@@ -10,7 +34,6 @@ function _gen_regular_lattice(dx::T, dy, ds; x0=zero(T), y0=zero(T), M::Int=100,
     return gen
 end
 
-# Hex Lattice
 """
 	gen_hex_lattice(spacing, direction = :pointy; kwargs...)
 Generate a hexagonal lattice of points with equal distance `spacing` between neighboring points.
@@ -49,7 +72,7 @@ function gen_hex_lattice(spacing, direction=:pointy, f::Function=identity; kwarg
 end
 
 """
-    _adapted_icogrid
+    _adapted_icogrid(radius::Number; correctionFactor=6/5)
 
 The `_adapted_icogrid` function generates an icosahedral grid with a specified
 radius. It defines the separation angle for the icosahedral grid using a
@@ -78,19 +101,31 @@ function _adapted_icogrid(radius::Number; correctionFactor=6/5)
     return icogrid(; sepAng)
 end
 
-function gen_cell_layout(region::GlobalRegion, radius::Number, type::ICO)
-    return _adapted_icogrid(radius; correctionFactor=type.correction)
-end
+"""
+    gen_cell_layout(region::GeoRegion, radius::Number, type::HEX; kwargs_lattice...)
+    gen_cell_layout(region::PolyRegion, radius::Number, type::HEX; kwargs_lattice...)
 
-function gen_cell_layout(region::GlobalRegion, radius::Number, type::H3)
-    error("H3 tassellation is not yet implemented in this version...")
-end
+The `gen_cell_layout` function generates a hexagonal cell layout for a given
+geographical region. It calculates the cell grid layout centered around the
+centroid of the main area of the region and returns the points within the
+specified region.
 
-function gen_cell_layout(region::LatBeltRegion, radius::Number, type::H3)
-    error("H3 tassellation is not yet implemented in this version...")
-end
+## Arguments
+- `region::Union{GeoRegion, PolyRegion}`: The geographical region for which the cell layout is \
+generated. Larger regions like global and LatBeltRegions are not supported because of the \
+problem of regular tassellation of the sphere.
+- `radius::Number`: The radius of each hexagonal cell. Has to be intended as the circumscribed \
+circumference.
+- `type::HEX`: A parameter indicating the type of lattice (only HEX is \
+supported).
+- `kwargs_lattice...`: Additional keyword arguments passed to the \
+`gen_hex_lattice` function.
 
-function gen_cell_layout(region::GeoRegion, radius::Number, type::HEX; hex_direction::Symbol=:pointy, kwargs_lattice...)
+## Returns
+- `Array{SimpleLatLon,1}`: An array of points (`SimpleLatLon`) representing the \
+cell centers within the specified region.
+"""
+function gen_cell_layout(region::GeoRegion, radius::Number, type::HEX; kwargs_lattice...)
     ## Find the domain center as seed for the cell grid layout.
     domain = extract_countries(region)[1] # The indicization is used to extract directly the Multi or PolyArea from the view
     centre = let
@@ -108,7 +143,7 @@ function gen_cell_layout(region::GeoRegion, radius::Number, type::HEX; hex_direc
 
     ## Generate the lattice centered in 0,0.
     spacing = radius * √3 / constants.Re_mean # spacing (angular in rad) between lattice points, considering a sphere of radius equivalent to the Earth mean radius.
-    lattice = gen_hex_lattice(spacing, hex_direction, rad2deg; kwargs_lattice...)
+    lattice = gen_hex_lattice(spacing, type.direction, rad2deg; kwargs_lattice...)
 
     ## Re-center the lattice around the seed point.
     new_lattice = map(lattice) do point
@@ -120,11 +155,7 @@ function gen_cell_layout(region::GeoRegion, radius::Number, type::HEX; hex_direc
     return filter_points(new_lattice, region)
 end
 
-function gen_cell_layout(region::GeoRegion, radius::Number, type::H3)
-    error("H3 tassellation is not yet implemented in this version...")
-end
-
-function gen_cell_layout(region::PolyRegion, radius::Number, type::HEX)
+function gen_cell_layout(region::PolyRegion, radius::Number, type::HEX; kwargs_lattice...)
     ## Find the domain center as seed for the cell grid layout.
     centre = centroid(region.domain)
     (; x, y) = centre.coords
@@ -132,7 +163,7 @@ function gen_cell_layout(region::PolyRegion, radius::Number, type::HEX)
 
     ## Generate the lattice centered in 0,0.
     spacing = radius * √3 / constants.Re_mean # spacing (angular in rad) between lattice points, considering a sphere of radius equivalent to the Earth mean radius.
-    lattice = gen_hex_lattice(spacing, hex_direction, rad2deg; kwargs_lattice...)
+    lattice = gen_hex_lattice(spacing, type.direction, rad2deg; kwargs_lattice...)
 
     ## Re-center the lattice around the seed point.
     new_lattice = map(lattice) do point
@@ -142,6 +173,36 @@ function gen_cell_layout(region::PolyRegion, radius::Number, type::HEX)
     end
 
     return filter_points(new_lattice, region)
+end
+
+"""
+    gen_cell_layout(region::GlobalRegion, radius::Number, type::ICO)
+    gen_cell_layout(region::Union{LatBeltRegion, GeoRegion, PolyRegion}, radius::Number, type::ICO)
+
+The `gen_cell_layout` function generates a cell layout using an icosahedral grid
+for a given geographical region. The function adapts the grid based on the
+specified radius and applies a correction factor (see `_adapted_icogrid`). 
+The radius as to be intended as the semparation angle of the point on the 
+icosahedral grid. It then filters the grid points to include only those within 
+the specified region.
+
+## Arguments
+- `region::Union{LatBeltRegion, GeoRegion, PolyRegion}`: The geographical region \
+for which the cell layout is generated. This can be a `LatBeltRegion`, \
+`GeoRegion`, or `PolyRegion`.
+- `radius::Number`: The radius used to adapt the icosahedral grid.
+- `type::ICO`: An object specifying the type of icosahedral grid and its \
+correction factor.
+
+## Returns
+- `Array{PointType, 1}`: An array of points representing the cell centers within \
+the specified region. The exact type of `PointType` depends on the \
+implementation of the `_adapted_icogrid` and `filter_points` functions.
+
+See also: [`_adapted_icogrid()`](@ref), [`icogrid()`](@ref)
+"""
+function gen_cell_layout(region::GlobalRegion, radius::Number, type::ICO)
+    return _adapted_icogrid(radius; correctionFactor=type.correction)
 end
 
 function gen_cell_layout(region::Union{LatBeltRegion, GeoRegion, PolyRegion}, radius::Number, type::ICO)
@@ -150,6 +211,6 @@ function gen_cell_layout(region::Union{LatBeltRegion, GeoRegion, PolyRegion}, ra
     return filter_points(grid, region)
 end
 
-function gen_cell_layout(region::PolyRegion, radius::Number, type::H3)
+function gen_cell_layout(region::Union{GlobalRegion, LatBeltRegion, GeoRegion, PolyRegion}, radius::Number, type::H3)
     error("H3 tassellation is not yet implemented in this version...")
 end
